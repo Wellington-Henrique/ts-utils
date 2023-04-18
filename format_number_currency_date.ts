6f551b467f6558eacac0f118c30adcf80a6245ef
Wellington Henrique <wellingtonhlc@gmail.com>
2023-04-17 22:54:36 -0300

    first commit

---
+++ format_number_currency_date.ts
@@ -0,0 +1,28 @@
+export const numberFormatterCompact = (value: number, locale='pr-BR') => {
+	return new Intl.NumberFormat(locale, {
+		notation: 'compact'
+	}).format(value);
+}
+
+export const currencyFormatter = (currency: number, locale='pr-BR') => {
+	return new Intl.NumberFormat(locale, {
+		currency: 'BRL',
+		style: 'currency'
+	}).format(currency);
+};
+
+export const dateFormatter = (date: Date, locale='pr-BR') => {
+	return new Intl.DateTimeFormat(locale, {
+		dateStyle: 'short'
+		//day: '2-digit',
+		//month: '2-digit',
+		//month: 'long',
+		//year: 'numeric',
+	}).format(date);
+};
+
+export const relativeDateFormatter = (amount: number, date: Date, locale='pr-BR') => {
+	return new Intl.RelativeTimeFormat(locale, {
+		numeric: 'auto'
+	}).format(amount, 'day');
+};
